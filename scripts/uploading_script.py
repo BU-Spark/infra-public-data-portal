d64024827798cfc8bdc0b16b1cc4e0f4be92ffb6
mmiah <mmiah@bu.edu>
2022-11-03 21:32:23 -0400

    initial script, added notes

--- scripts/uploading_script.py
+++ scripts/uploading_script.py
@@ -8,3 +8,6 @@ for filename in os.listdir (directory):
                   data={"package_id":"my_dataset"},
                   headers={"X-CKAN-API-Key": "21a47217-6d7b-49c5-88f9-72ebd5a4d4bb"},
                   files=[('upload', file(directory + filename))])
+    # code from https://docs.ckan.org/en/2.9/maintaining/filestore.html
+    # not sure if the part that says 'data={"package_id":"my_dataset"}' needs to be customized 
+    # with our own info
