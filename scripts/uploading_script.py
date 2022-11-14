7703b39123c9c3ef2f99934d9637fce85490862c
mmiah <mmiah@bu.edu>
2022-11-14 10:24:33 -0500

    wrote 2 more functions: find_dataset() which looks to see if a dataset with a given filename has already been posted and find_tagged_datasets() which returns a list of all of the datasets with a given tag

--- scripts/uploading_script.py
+++ scripts/uploading_script.py
@@ -45,13 +45,19 @@ def add_dataset (metadata_json):
    all_files = data["File Names"]
    file_names = all_files.split (", ")
    for data_file in file_names: 
-     requests.post('http://0.0.0.0:5000/api/action/resource_create',
-                    data={"package_id":"my_dataset"},
-                    headers={"X-CKAN-API-Key": API_TOKEN},
-                    files=[('upload', file(f"data/{data_file}"))])
+      if not find_dataset (data_file): # only post the data if it is not already on the site 
+         requests.post(f'http://{SITE_URL}/api/action/resource_create',
+                     data={"package_id":"my_dataset"},
+                     headers={"X-CKAN-API-Key": API_TOKEN},
+                     files=[('upload', file(f"data/{data_file}"))])
+      else: 
+         # remove the dataset already on the site and upload the new one 
+         pass 
+
    print (data) 
 
 def get_groups (): 
+   # gets the list of all the groups currently on the website 
    response = urllib.request.urlopen (SITE_URL + "/api/3/action/group_list")
    response_dict = json.loads(response.read()) 
 
@@ -62,17 +68,35 @@ def get_groups ():
       print (response_dict['error'])
 
 def get_datasets (): 
+   # gets the list of all the datasets that are already on the website 
    response = urllib.request.urlopen (SITE_URL + "/api/3/action/package_list")
    response_dict = json.loads(response.read()) 
 
    if response_dict['success']: 
-      print (response_dict['result'])
+      # print (response_dict['result'])
       return response_dict['result']
    else: 
       print (response_dict['error'])
+
+def find_tagged_datasets (tag): 
+   # returns a list of all of the datsets on the site that have the given tag 
+   response = urllib.request.urlopen (SITE_URL + f"/api/3/action/package_search?fq=tags:{tag}")
+   response_dict = json.loads(response.read()) 
+
+   results = response_dict['result']['results']
+   
+   return results 
+
+def find_dataset (name_of_dataset): 
+   # given the name of a dataset, looks to see if that dataset is already uploaded on the site 
+   datasets = get_datasets()
+   return name_of_dataset in datasets
+
+
 
 # add_dataset ("jsons/row1.json")
 # c = pd.read_csv ("https://github.com/BU-Spark/summer2021internship/blob/master/Police%20Arrest%20Analysis/cumulativeNIBRS.csv", on_bad_lines='skip')
 # print (c)
 
-get_datasets() 
+# print (find_dataset ("mijente-fall-2020"))
+find_tagged_datasets ("geojson")
