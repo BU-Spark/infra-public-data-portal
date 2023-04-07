5b03cc4f10e349b09752a9947a10406d281a4770
jamxiao2025 <95207937+jamxiao2025@users.noreply.github.com>
2023-04-07 01:24:27 -0400

    package_updater supported with argparse..add comments tomorrow

---
+++ ckan-scripts/package_update.py
@@ -0,0 +1,109 @@
+import argparse
+import json
+import os
+import requests
+from dotenv import load_dotenv
+import ckanapi
+
+load_dotenv()
+
+MY_API_KEY = os.getenv("API_KEY")
+HEADERS = {
+    'Authorization': MY_API_KEY
+}
+
+# If the package name exists, then this function will return the package ID as well as its resource's names and IDs 
+def get_package_data(name):
+    ckan = ckanapi.RemoteCKAN('http://data.buspark.io', apikey=MY_API_KEY)
+    packages = ckan.action.current_package_list_with_resources()
+    
+    try:
+        package_data = None
+        resources = {}
+        for package in packages:
+            if package['name'] == name:
+                package_data = package
+                break
+        if package_data is None:
+            raise KeyError("Package does not exist")
+        
+        package_id = package_data['id']
+        for resource in package_data['resources']:
+            resources[resource['name']] = resource['id']
+        
+        return (package_id, resources)
+    except KeyError as e:
+        print(e)
+
+        
+def package_revise(package_id, resources, update_metafields, resource_updates):
+    print(update_metafields)
+    data_dict = {}
+    data_dict['match'] = f'{{"id": "{package_id}"}}'
+    
+    if 'Remove' in update_metafields:
+        removals = update_metafields['Remove']
+        if len(removals) > 0:
+            data_dict['filter'] = json.dumps(removals)
+    
+    if 'Update' in update_metafields:
+        updates = update_metafields['Update']
+        if len(updates) > 0:
+            data_dict['update'] = json.dumps(updates)
+    
+    if len(resource_updates) > 0:
+        for name, data in resource_updates.items():
+            if name in resources:
+                resource_id = resources[name]
+                index = 'update__resources__'  + resource_id[0:6]
+                data_dict[index] = json.dumps(data)
+    
+    print(data_dict)
+    return data_dict
+
+
+def dictionary(a):
+    try:
+        data = json.loads(a)
+        return data
+    except ValueError as e:
+        print("Error: Please provide a json object in string format ")
+        data = None
+    
+    return data
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "package_name",
+    help="name of the package you want to update",
+    type=str
+)
+
+parser.add_argument(
+    "update_metafields",
+    help="metafields that you want to update. See https://docs.ckan.org/en/2.10/api/index.html#module-ckan.logic.action.create for list of fields",
+    type=dictionary
+)
+
+parser.add_argument(
+    "resources",
+    help="metafields that you want to clear. See https://docs.ckan.org/en/2.10/api/index.html#module-ckan.logic.action.create for list of fields",
+    type=dictionary
+)
+
+args = parser.parse_args()
+package_name = args.package_name
+update = args.update_metafields
+resources = args.resources
+print(resources)
+
+#now we have the ID and the Resources
+package_id, resource_list = get_package_data(package_name)
+#insert the package_id and resource_list into our function
+data_dict = package_revise(package_id, resource_list, update, resources)
+#make API call
+REVISE_URL = 'http://data.buspark.io/api/3/action/package_revise'
+response = requests.post(REVISE_URL, data=data_dict, headers=HEADERS)
+response_dict = response.json()
+print(response_dict)
--- ckan-scripts/update_package.py
+++ ckan-scripts/update_package.py
@@ -7,8 +7,8 @@ MY_API_KEY = os.getenv("API_KEY")
 HEADERS = {
     'Authorization': MY_API_KEY
 }
-PACKAGE_ID = 'd874af6c-cd54-4c4a-a05d-504df04e1df2'                 # In the future we would like to pass package ID or name as a parameter. 
-URL = 'http://data.buspark.io/api/3/action/package_revise'          # API endpoint.
+PACKAGE_ID = 'd874af6c-cd54-4c4a-a05d-504df04e1df2'                 # In the future we would like to pass package ID or name as a parameter 
+URL = 'http://data.buspark.io/api/3/action/package_revise'          # API endpoint
 # id, name, title, notes, url, version, author, author_email, maintainer,
 #  maintainer_email, license_id, private, owner_org, state, type, resources, tags, groups, extras
 # put the details of the dataset we're going to create into a dict
@@ -17,7 +17,10 @@ data_dict = {
     'update': '{"notes": "Collecting a record of items of personal property, \
                           such as equipment and supplies, for which state agencies \
                           no longer have a business use, but that have some reuse value. ",\
-                "title": "state_surplus"}'
+                "title": "state_surplus"\
+                }',
+        'update__resources__dfaf6f' : '{"name": "parcel-geoData"}'
+
 }
 # use the json module to dump the dictionary to a string for posting, encoding the URL
 # creating a dataset requires an authorization header
