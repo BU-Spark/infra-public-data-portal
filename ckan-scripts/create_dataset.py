cc6c9da6faba1c14bd88b8ce035779c3735f0372
ayang903 <98380739+ayang903@users.noreply.github.com>
2023-07-02 23:33:59 -0400

    Update create_dataset.py
    update create_dataset with new json fields

--- ckan-scripts/create_dataset.py
+++ ckan-scripts/create_dataset.py
@@ -3,49 +3,49 @@ import requests
 import sys
 import os
 import json
-from dotenv import load_dotenv
-load_dotenv()
 
 MY_API_KEY = os.getenv("API_KEY")
 
 dsCreator = 'riskEvaluator.json'
-#read JSON file 
 
-f = open(os.path.join(sys.path[0],'jsonf',dsCreator))
-data = json.load(f) #converts json object to python dict
+f = open(dsCreator)
+data = json.load(f)
 
-package_name = data['projectName']
+package_name = data['project_name']
 package_ds = data['description']
-package_src_url = data['url']
-package_rf = data['resourceFolder']
+package_more_information = data['more_information']
+package_uris = data['uris']
 package_org = data['owner_org']
 
 ckan = ckanapi.RemoteCKAN('http://data.buspark.io', apikey=MY_API_KEY)
 
-# This follows the example provided in ckanapi repo: Create the "Sample" dataset.
 try:
-    print('Creating {package_title}')
-    package = ckan.action.package_create(name=package_name, notes=package_ds, url = package_src_url, owner_org=package_org)
+    print(f'Creating {package_name}')
+    package = ckan.action.package_create(name=package_name, notes=package_ds, url = package_more_information, owner_org=package_org)
 except ckanapi.ValidationError as e:
-    if (e.error_dict['__type'] == 'Validation Error' and
-       e.error_dict['name'] == ['That URL is already in use.']):
-        print('"{package_title}" package already exists'.format(**locals()))
-        package = ckan.action.package_show(id=package_name)
-        print(package)
+    if e.error_dict.get('__type') == 'Validation Error':
+        if 'name' in e.error_dict and e.error_dict['name'] == ['That URL is already in use.']:
+            print(f'"{package_name}" package already exists')
+            package = ckan.action.package_show(id=package_name)
+            print(package)
+        elif 'owner_org' in e.error_dict:
+            print(f"Error: {e.error_dict['owner_org'][0]}")
     else:
         raise
+print('------------------------------------')
+print(package['id'])
 
-for filename in os.listdir(os.path.join(sys.path[0],package_rf)):
-    csv_file = open(os.path.join(sys.path[0],package_rf,filename), "rb")
-    path = os.path.join(sys.path[0],package_rf,filename)
+for filename in os.listdir(os.path.join(sys.path[0],package_uris)):
+    csv_file = open(os.path.join(sys.path[0],package_uris,filename), "rb")
+    path = os.path.join(sys.path[0],package_uris,filename)
     extension = os.path.splitext(filename)[1][1:].upper()
-    resource_name = filename[0:-4] #remove extension from resource name
+    resource_name = filename[0:-4] 
     print('Creating "{resource_name}" resource'.format(**locals()))
     r = requests.post('http://data.buspark.io/api/3/action/resource_create',
                       data={'package_id': package['id'],
                             'name': resource_name,
                             'format': extension,
-                            'url': 'upload',  # Needed to pass validation
+                            'url': 'upload',  
                             },
                       headers={'Authorization': MY_API_KEY},
                       files={"upload": csv_file})
